f4cc4fd73f8f1dc5a7371a3cfd82e1d4a81fc318
Céline MAGNERON <59822501+CelineMAGNERON@users.noreply.github.com>
2020-04-15 19:20:43 +0200

    rollback

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="index.css">
-    <link rel="stylesheet" href="z.css">
     <meta name="viewport" content="width=device-width, initial-scale=1"
         content="Al Mamounia et ses enseignements. Ce site propose de partir à la découverte d'une philosophie laïque et spirituelle.">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
